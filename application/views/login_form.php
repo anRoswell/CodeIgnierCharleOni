c7d042a2af7eb8af4a5a5c649ca466c757785421
Alfonso Navarro <anroswell@gmail.com>
2020-01-27 21:43:13 -0500

    second commit

---
+++ application/views/login_form.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SB Admin 2 - Bootstrap Admin Theme</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="<?php echo base_url();?>/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="<?php echo base_url();?>/vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="<?php echo base_url();?>/dist/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="<?php echo base_url();?>/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <div class="login-panel panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Please Sign In</h3>
+                    </div>
+                    <div class="panel-body">
+                        <?php
+                            //Print logout message
+                            if (isset($message_display)) {
+                                echo "<div class='alert alert-info'>";
+                                echo $message_display;
+                                echo "</div>";
+                            }
+
+                            //Print input data error message
+                            if (isset($error_message)) {
+                                echo "<div class='alert alert-danger'>";
+                                echo $error_message;
+                                echo "</div>";
+                            }
+
+                            if (null !== validation_errors() && strlen(validation_errors()) > 0) {
+                                echo "<div class='alert alert-warning'>";
+                                echo validation_errors();
+                                echo "</div>";
+                            }
+                        ?>
+
+                        <?php echo form_open('user_authentication/user_login_process',array('role'=>'form')); ?>
+                            <fieldset>
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="Username" name="username" type="username" value="<?php echo set_value('username'); ?>" autofocus>
+                                </div>
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="Password" name="password" type="password" value="">
+                                </div>
+                                <div class="checkbox">
+                                    <label>
+                                        <input name="remember" type="checkbox" value="Remember Me">Remember Me
+                                    </label>
+                                </div>
+
+                                <button class="btn btn-lg btn-success btn-block" name="submit" type="submit" value="">Login</button>
+                            </fieldset>
+                            <?php echo form_close(); ?>
+                        <br/>
+                        <a href="<?php echo base_url(); ?>index.php/user_authentication/user_registration_show" class="btn btn-sm btn-info btn-block">To SignUp Click Here</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="<?php echo base_url();?>/vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?php echo base_url();?>/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="<?php echo base_url();?>/vendor/metisMenu/metisMenu.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="<?php echo base_url();?>/dist/js/sb-admin-2.js"></script>
+
+</body>
+
+</html>
---
+++ application/views/main.php
@@ -0,0 +1,10 @@
+html5<!DOCTYPE html>
+  <head>
+    <title>Informes Uncle Charles</title>
+    <link rel="shortcut icon" type="image/x-icon" href="./assets/favicon.ico" />
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+  </head>
+  
+  <body></body>
+</html>
